47451937704b5f5e7679c20d060cf51cfdfc37e5
KaranJayakumar <karanjaykumar@gmail.com>
2025-10-29 16:51:59 +1100

    Disable formatter temp

--- lua/Karan/plugins/formatter.lua
+++ lua/Karan/plugins/formatter.lua
@@ -1,27 +1,2 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
-    conform.setup({
-      formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        lua = { "stylua" },
-        python = { "ruff" }
-      },
-      format_on_save = {
-        lsp_fallback = true,
-      },
-    })
-    vim.keymap.set("n", "<leader>cc", function()
-      conform.format({
-      })
-    end)
-  end,
 }
