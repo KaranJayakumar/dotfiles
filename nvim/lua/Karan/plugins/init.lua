9e7585adf459e25ab4a21485a76987ec1104be19
KaranJayakumar <karanjaykumar@gmail.com>
2026-01-04 00:30:26 +1100

    Update config add some telescope find bufs

--- nvim/lua/Karan/plugins/init.lua
+++ nvim/lua/Karan/plugins/init.lua
@@ -32,6 +32,7 @@ return {
 		config = function()
 			local builtin = require('telescope.builtin')
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fj', builtin.buffers, {})
 			vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 			vim.keymap.set('n', '<leader>ft', function()
 				builtin.grep_string({ search = vim.fn.input("Grep > ") });
